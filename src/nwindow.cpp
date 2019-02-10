50b4f087dc6dd8577d241a17391274566de4df0c
holderekt <navispeshd@gmail.com>
2019-02-10 23:23:41 +0100

    CMake and first window using OpenGL

---
+++ src/nwindow.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main(){
+
+
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Day 1", NULL,NULL);
+    if(window == NULL){
+        std::cout << "Cant do shit" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    std::cout << "Cant initialize GLAD" << std::endl;
+    return -1;
+    }
+
+    // Tell OpenGL the rendering window
+    glViewport(0,0,800,600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Main Loop
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+// Function that handles the resize at runtime
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0,width, height);    
+}
