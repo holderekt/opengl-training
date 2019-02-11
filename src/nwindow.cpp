f238ec0fe04b5ad76b490b661cf670f9794be7a1
holderekt <navispeshd@gmail.com>
2019-02-11 22:49:15 +0100

    vertex triangle render

--- src/nwindow.cpp
+++ src/nwindow.cpp
@@ -3,10 +3,15 @@
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main(){
 
-
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f
+    };
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -14,6 +19,7 @@ int main(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Day 1", NULL,NULL);
+
     if(window == NULL){
         std::cout << "Cant do shit" << std::endl;
         glfwTerminate();
@@ -31,10 +37,23 @@ int main(){
     glViewport(0,0,800,600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
     // Main Loop
     while(!glfwWindowShouldClose(window)){
+
+        processInput(window);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        glClearColor(0.8f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
     }
 
     glfwTerminate();
@@ -45,4 +64,10 @@ int main(){
 // Function that handles the resize at runtime
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0,0,width, height);    
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
